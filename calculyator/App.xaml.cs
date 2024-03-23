d62a73015066e1c7416d75306c11bee847dd5255
klopovaevgeniya <isip_e.d.klopova@mpt.ru>
2024-03-23 13:30:14 +0300

    Добавьте файлы проекта.

---
+++ calculyator/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using calculyator;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace calculyator
+{
+    public partial class App : Application
+    {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+
+            CalculatorModel calculator = new CalculatorModel();
+            CalculatorViewModel viewModel = new CalculatorViewModel(calculator);
+            window.DataContext = viewModel;
+
+            window.Show();
+        }
+    }
+}
